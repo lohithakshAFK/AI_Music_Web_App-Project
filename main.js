9026dfbf723a93284d2fcd7476e2ba87bdb5a878
Lohithaksh <83767927+lohithakshAFK@users.noreply.github.com>
2021-11-10 22:31:21 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
+centuries = "";
+believer = "";
+
+function preload(){
+    centuries = loadSound("iron man - centuries.mp3");
+    believer = loadSound("believer.mp3");
+}
+
 function setup(){
-    canvas = createCanvas(250,250);
+    canvas = createCanvas(500,300);
     canvas.center();
     video = createCapture(VIDEO);
     video.hide();
 }
 
 function draw(){
-    image(video,0,0,250,250)
+    image(video,0,0,500,300);
 }
